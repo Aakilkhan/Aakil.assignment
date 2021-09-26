53aaf12458208a67711fea1e8ace157904ede151
Aakilkhan <43509100+Aakilkhan@users.noreply.github.com>
2021-09-26 21:19:52 +0530

    Add files via upload

---
+++ digit count/src/sum/com/Main.java
@@ -0,0 +1,20 @@
+package sum.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+         int n = 265;
+                int count = 0;
+                while(n !=0){
+                    n =  n/10;
+
+                    count++;
+                }
+                System.out.print(count);
+            }
+        }
+
+
---
+++ prime no/src/prime/com/Main.java
@@ -0,0 +1,14 @@
+package prime.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 23;
+        if(n%2==0){
+            System.out.print("the no is prime");
+        }else{
+            System.out.print("the no s not prime");
+        }
+    }
+}
+ 
