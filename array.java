3112814b4cadaf50d163233bd2238533871dbd6e
Aakilkhan <43509100+Aakilkhan@users.noreply.github.com>
2021-09-27 21:30:08 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+public class MaxNumArray {
+
+	public static void main(String[] args) {
+		
+		int n[] = {1,80,,45,60,85,40,3};
+		int max = 0;
+		
+		for(int i = 0 ; i < n.length; i++ ) {
+			if (n[i] > largest) {
+				max =  n[i];
+			}
+			
+		}
+		System.out.println(max+" is the greatest number" );
+
+	}
+
+}
---
+++ evenodd.java
@@ -0,0 +1,13 @@
+public class EvenOddNUM {
+
+	public static void main(String[] args) {
+		int n = 63;
+		if (n % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
